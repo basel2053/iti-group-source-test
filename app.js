79a58f7710c9bc7c76336582fcd86567422ba8b4
yahia <mohamedyahia207545@gmail.com>
2023-01-28 14:58:26 +0200

    edit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-	console.log('I wanna sleepz');
+app.get("/", (req, res) => {
+  console.log("I wanna sleepz");
 });
 app.listen(3000);
-console.log('YahiA');
-console.log('Bassel');
+console.log("Yahia");
+console.log("Bassel");
