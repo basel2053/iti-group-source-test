59ef0eb3cf7e1c9455932b693aaa62ad2c4722c3
Bishoy Osama <bishoy.o.wadie@gmail.com>
2023-01-28 15:06:23 +0200

    about API

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+const { log } = require('console');
 const express = require('express');
+const { get } = require('http');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log('I wanna sleepz');
+});
 
+app.get('/about', (req, res) => {
+    console.log('msh fahim 7aga');
+})
 app.listen(3000);
